30cea00daab969992174f4af20dd7d0ef35af534
Ronaksengupta99 <55063393+r1999-ron@users.noreply.github.com>
2024-05-30 23:00:00 +0530

    Update ChatController.scala

--- app/controllers/ChatController.scala
+++ app/controllers/ChatController.scala
@@ -24,13 +24,57 @@ class ChatController @Inject()(
 
   kafkaConsumer.receiveMessages()
 
+  /*def chatPage = Action { implicit request =>
+    println("Inside chatPage method")
+    println("Session data:")
+    println(request.session.data)
+    request.session.get("username").map { username =>
+      println(s"Username found in session: $username")
+      Ok(views.html.chat(username))
+    }.getOrElse {
+      println("Username not found in session")
+      Unauthorized("You are not logged in")
+    }
+  }*/
+  /*def chatPage = Action { implicit request =>
+    val currentTime = System.currentTimeMillis()
+    val sessionExpiry = request.session.get("expiry").flatMap(_.toLongOption)
+
+    println(s"Current time: $currentTime")
+    println(s"Session expiry time: $sessionExpiry")
+
+    println("Session data: " + request.session.data)
+
+    if (sessionExpiry.exists(_ < currentTime)) {
+      println("Session expired. Redirecting to login page.")
+      Redirect("http://localhost:9299/login").withNewSession
+    } else {
+      request.session.get("username").map { username =>
+        println(s"User $username is logged in.")
+        Ok(views.html.chat(username))
+      }.getOrElse {
+        println("User is not logged in.")
+        Unauthorized("You are not logged in")
+      }
+    }
+  }*/
+
+  def chatPage = Action { implicit request =>
+    request.getQueryString("username").map { username =>
+      Ok(views.html.chat(username))
+    }.getOrElse {
+      Redirect("http://localhost:9299/login")
+    }
+  }
+
+
   def sendMessage = Action.async(parse.json) { implicit request =>
     request.body.validate[Message].fold(
       errors => {
         Future.successful(BadRequest(Json.obj("status" -> "error", "message" -> JsError.toJson(errors))))
       },
       sendMessageRequest => {
-        kafkaProducer.sendMessage(sendMessageRequest.senderId, sendMessageRequest.receiverId, sendMessageRequest.content, sendMessageRequest.timestamp).map{_ =>
+        kafkaProducer.sendMessage(sendMessageRequest.senderId, sendMessageRequest.receiverId, sendMessageRequest.content, sendMessageRequest.timestamp).map { _ =>
           Ok(Json.obj("status" -> "Message sent"))
         }
       }
@@ -43,7 +87,7 @@ class ChatController @Inject()(
         Future.successful(BadRequest(Json.obj("status" -> "error", "message" -> JsError.toJson(errors))))
       },
       sendMessageRequest => {
-        kafkaProducer.sendMessage(sendMessageRequest.senderId, sendMessageRequest.receiverId, sendMessageRequest.content, sendMessageRequest.timestamp).map{_ =>
+        kafkaProducer.sendMessage(sendMessageRequest.senderId, sendMessageRequest.receiverId, sendMessageRequest.content, sendMessageRequest.timestamp).map { _ =>
           Ok(Json.obj("status" -> "Message sent"))
         }
       }
@@ -51,23 +95,12 @@ class ChatController @Inject()(
   }
 
   def fetchMessages(userId: String) = Action.async { implicit request =>
-    dbService.getMessagesForUser(userId).map { messages =>
-      val messageContents = messages.map { message =>
-        Json.obj("content" -> message.content)
+    if (userId.trim.isEmpty) {
+      Future.successful(BadRequest("User ID is missing"))
+    } else {
+      dbService.getMessagesForUser(userId).map { messages =>
+        Ok(Json.toJson(messages))
       }
-      Ok(Json.toJson(messageContents))
     }
-  }
-
-  def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
-  }
-
-  def sender() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.sender())
-  }
-
-  def receiver() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.receiver())
   }
 }
